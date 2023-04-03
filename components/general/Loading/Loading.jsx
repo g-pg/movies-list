b997896cee60aaf6f02aa2cafe9165ccd15bba51
g-pg <gabrielpgusso@gmail.com>
2023-04-03 17:11:11 -0300

    refactor to use svgr

--- components/general/Loading/Loading.jsx
+++ components/general/Loading/Loading.jsx
@@ -1,18 +1,13 @@
 import React from "react";
-import Image from "next/image";
 import LoadingSvg from "@/public/loading.svg";
 export default function Loading() {
 	return (
-		<p>
-			<Image
-				src={LoadingSvg}
-				alt="Loading..."
-				style={{
-					margin: "auto",
-					inset: "0",
-					position: "absolute",
-				}}
-			></Image>
-		</p>
+		<LoadingSvg
+			style={{
+				margin: "auto",
+				inset: "0",
+				position: "absolute",
+			}}
+		/>
 	);
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	// reactStrictMode: true,
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/i,
+			issuer: /\.[jt]sx?$/,
+			resourceQuery: { not: /url/ }, // exclude if *.svg?url
+			use: ["@svgr/webpack"],
+		});
+		return config;
+	},
 	images: {
 		remotePatterns: [
 			{
