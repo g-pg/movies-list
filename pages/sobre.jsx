2faae63e39f645718e5eec4b1a051be8391b0877
g-pg <gabrielpgusso@gmail.com>
2023-04-04 14:42:41 -0300

    created about page

---
+++ pages/sobre.jsx
@@ -0,0 +1,58 @@
+import PageParagraph from "@/components/PageParagraph/PageParagraph";
+import PageTitle from "@/components/general/PageTitle/PageTitle";
+import PrimaryLayout from "@/components/general/PrimaryLayout/PrimaryLayout";
+import React from "react";
+
+export default function sobre() {
+	return (
+		<>
+			<PrimaryLayout>
+				<div className="container">
+					<PageTitle>Sobre</PageTitle>
+
+					<PageParagraph>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint
+						repellendus, aspernatur ducimus eaque corrupti est accusamus qui
+						veniam consequatur, explicabo vel, dolorem magnam eos. Rem id hic aut
+						maiores alias voluptatibus ipsum natus et amet, at inventore omnis a
+						quo commodi earum, doloribus culpa eveniet! Culpa dolorum vitae
+						voluptas voluptates, iusto ullam veniam vero nisi ut aspernatur
+						provident, unde voluptatem possimus molestias ratione? Necessitatibus
+						ab architecto ut amet, sit temporibus!
+					</PageParagraph>
+					<PageParagraph>
+						Exercitationem, fuga ut recusandae quidem explicabo magni nam velit
+						libero maiores corporis? Error voluptatum quibusdam quidem officia
+						dicta consequuntur esse ut reprehenderit similique, nisi mollitia quam
+						at facilis natus hic repellat molestiae labore deleniti optio enim
+						accusantium et dolor? Consequuntur quas vero eius? Quo cumque neque
+						sit officiis a sunt esse rem odit aperiam consequuntur, placeat
+						mollitia minus vero error similique aliquam, ad maiores! Accusamus
+						enim tempora fugit tempore sunt?
+					</PageParagraph>
+					<PageParagraph>
+						Repellat minus exercitationem cupiditate veritatis, laudantium
+						consectetur aperiam, provident, modi voluptatum vitae optio at nostrum
+						quaerat quo ratione itaque laboriosam! Eaque fugit nostrum animi
+						laudantium, repudiandae in dicta non, molestias, quae nulla optio?
+						Corrupti aperiam recusandae voluptatum eaque nemo, perferendis, enim,
+						quaerat exercitationem tempore temporibus ad quod excepturi neque
+						esse. Nam optio, quis eius cumque, omnis quia a vero saepe numquam
+						suscipit ratione nesciunt adipisci ea repellendus dolore minima
+						accusamus.
+					</PageParagraph>
+					<PageParagraph>
+						Dolorum provident cumque, mollitia rerum veritatis temporibus culpa
+						enim aperiam dicta nesciunt minima. Sequi asperiores placeat sed
+						reprehenderit. Dolore tempora, natus magnam at nostrum blanditiis a
+						qui unde delectus provident. Laudantium, facere beatae. Esse, ducimus.
+						Esse sint sequi ipsum earum libero, ea voluptate. Laudantium nisi
+						libero, animi id quidem autem omnis consequuntur error molestias quas
+						doloremque culpa ullam quod in explicabo temporibus, accusamus
+						corporis repudiandae dignissimos hic cum atque nam?
+					</PageParagraph>
+				</div>
+			</PrimaryLayout>
+		</>
+	);
+}
