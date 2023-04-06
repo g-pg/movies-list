8f9b07819427e8f040dc95c91cb8674ee06cfa2c
g-pg <gabrielpgusso@gmail.com>
2023-04-06 03:55:06 -0300

    click to mousedown to avoid unintentional closing

--- hooks/useClickOutside.js
+++ hooks/useClickOutside.js
@@ -9,34 +9,10 @@ export default function useClickOutside(setShow) {
 				setShow(false);
 			}
 		}
-		document.addEventListener("click", handleClickOutside, true);
+		document.addEventListener("mousedown", handleClickOutside, true);
 
-		return () => document.removeEventListener("click", handleClickOutside, true);
+		return () => document.removeEventListener("mousedown", handleClickOutside, true);
 	}, [ref, setShow]);
 
 	return { ref };
 }
-
-// import { useEffect, useRef } from "react";
-
-// export default function useHideElement(setShow) {
-// 	const ref = useRef(null);
-
-// 	function handleClickOutside(event) {
-// 		if (ref.current && !ref.current.contains(event.target)) {
-// 			setShow(false);
-// 		}
-// 	}
-
-// 	useEffect(() => {
-// 		document.addEventListener("click", handleClickOutside, true);
-
-// 		return () => {
-// 			document.removeEventListener("click", handleClickOutside, true);
-// 		};
-// 	}, [ref]);
-
-// 	return (newRef) => {
-// 		ref.current = newRef;
-// 	};
-// }
