993e12c074aa0b332848bf4b8558255bc88c7601
g-pg <gabrielpgusso@gmail.com>
2023-04-06 04:44:29 -0300

    improved slider overlay

--- components/pages/Home/Slider/Slider.jsx
+++ components/pages/Home/Slider/Slider.jsx
@@ -5,29 +5,31 @@ import classNames from "classnames";
 export default function Slider({ popularMovies, style }) {
 	return (
 		<section className={styles.section} style={style}>
-			<div className={classNames(styles.slider)}>
-				{popularMovies.results.map((el) => {
-					return (
-						<Image
-							width="154"
-							height="231"
-							src={`https://image.tmdb.org/t/p/w154${el.poster_path}`}
-							alt={el.title}
-							key={el.id}
-						/>
-					);
-				})}
-				{popularMovies.results.map((el) => {
-					return (
-						<Image
-							width="154"
-							height="231"
-							src={`https://image.tmdb.org/t/p/w154${el.poster_path}`}
-							alt={el.title}
-							key={`${el.id + 1}`}
-						/>
-					);
-				})}
+			<div className={styles.wrapper}>
+				<div className={classNames(styles.slider)}>
+					{popularMovies.results.map((el) => {
+						return (
+							<Image
+								width="154"
+								height="231"
+								src={`https://image.tmdb.org/t/p/w154${el.poster_path}`}
+								alt={el.title}
+								key={el.id}
+							/>
+						);
+					})}
+					{popularMovies.results.map((el) => {
+						return (
+							<Image
+								width="154"
+								height="231"
+								src={`https://image.tmdb.org/t/p/w154${el.poster_path}`}
+								alt={el.title}
+								key={`${el.id + 1}`}
+							/>
+						);
+					})}
+				</div>
 			</div>
 		</section>
 	);
