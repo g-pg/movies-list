62677f850490daf2129c539ecb165eac16bcad3d
g-pg <gabrielpgusso@gmail.com>
2023-04-06 21:51:44 -0300

    handle search by ID or by name

--- pages/api/searchmovie.js
+++ pages/api/searchmovie.js
@@ -1,13 +1,18 @@
 import axios from "axios";
 
 export default async function handler(req, res) {
+	const { name, id } = req.query;
+	const KEY = process.env.MOVIEDB_KEY;
+	let URL;
+
+	if (name) {
+		URL = `https://api.themoviedb.org/3/search/movie?api_key=${KEY}&language=pt-BR&query=${name}`;
+	} else if (id) {
+		URL = `https://api.themoviedb.org/3/movie/${id.toString()}?api_key=${KEY}`;
+	}
+
 	try {
-		const { name } = req.query;
-		const KEY = process.env.MOVIEDB_KEY;
-		console.log(KEY);
-		const movie = await axios.get(
-			`https://api.themoviedb.org/3/search/movie?api_key=${KEY}&language=pt-BR&query=${name}`
-		);
+		const movie = await axios.get(URL);
 		return res.status(200).json(movie.data);
 	} catch (error) {
 		return res.status(500).json(error);
