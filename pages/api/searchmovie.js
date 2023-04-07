d353aa7911a3227cadd0233018ad36c82a3214e3
g-pg <gabrielpgusso@gmail.com>
2023-04-07 01:14:40 -0300

    map through ids in the server instead of client

--- pages/api/searchmovie.js
+++ pages/api/searchmovie.js
@@ -7,14 +7,25 @@ export default async function handler(req, res) {
 
 	if (name) {
 		URL = `https://api.themoviedb.org/3/search/movie?api_key=${KEY}&language=pt-BR&query=${name}`;
-	} else if (id) {
-		URL = `https://api.themoviedb.org/3/movie/${id.toString()}?api_key=${KEY}`;
-	}
 
-	try {
-		const movie = await axios.get(URL);
-		return res.status(200).json(movie.data);
-	} catch (error) {
-		return res.status(500).json(error);
+		try {
+			const movie = await axios.get(URL);
+			return res.status(200).json(movie.data);
+		} catch (error) {
+			return res.status(500).json(error);
+		}
+	} else if (id) {
+		const arrayOfIds = id.split(",");
+
+		try {
+			const moviePromises = await arrayOfIds.map((id) =>
+				axios.get(`https://api.themoviedb.org/3/movie/${id.toString()}?api_key=${KEY}`)
+			);
+			const responses = await Promise.all(moviePromises);
+			const data = responses.map((response) => response.data);
+			return res.status(200).json(data);
+		} catch (error) {
+			return res.status(500).json(error);
+		}
 	}
 }
